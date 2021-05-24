c15bdc4764b72355fa0fdefe20d49b11499158d0
kafribung <60900117001@uin-alauddin.ac.id>
2021-05-25 06:08:16 +0800

    cleanCode

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                <div class="p-6 text-xl text-center bg-white border-b border-gray-200">
+                    Selamat datang {{ Auth::user()->name }}
                 </div>
             </div>
         </div>
