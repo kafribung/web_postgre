b73107ef196c7caa84f9402bb0954c20cb3f843c
kafribung <60900117001@uin-alauddin.ac.id>
2021-05-26 11:02:01 +0800

    clean code

--- resources/views/admin/category/category.blade.php
+++ resources/views/admin/category/category.blade.php
@@ -8,9 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-xl text-center bg-white border-b border-gray-200">
-                    Category
-                </div>
+                
             </div>
         </div>
     </div>
